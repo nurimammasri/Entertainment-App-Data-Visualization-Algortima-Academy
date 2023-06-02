1bd2c27ef375cf2883a0ba20977150ff32a88f74
“nurimammasri” <nurimammasri.01@gmail.com>
2023-06-02 17:51:22 +0800

    Update Initial

--- server.R
+++ server.R
@@ -3,35 +3,36 @@ shinyServer(function(input, output) {
   #OUTPUT PAGE 1
   spotify_by_year <- spotify_clean %>% 
     group_by(year) %>%
-    summarise(across(c(-artists, -duration_ms, -explicit, -mode, -name, -release_date, -tempo, -id, -loudness, -popularity), \(x) mean(x, na.rm = TRUE)))
+    summarise(across(c(-artists, -duration_ms, -explicit, -mode, -name, -release_date, -tempo, -id, -loudness, -popularity), mean, na.rm = TRUE))
   
   #plot
   output$plot1 <- renderPlotly({
-    data_agg1 <- spotify_by_year %>% 
-      select(-key) %>% 
-      gather(key = "Variable", value = "Value", -year)
+  data_agg1 <- spotify_by_year %>% 
+    select(-key) %>% 
+    gather(key = "Variable", value = "Value", -year)
+
     
-    
-    plot1<- ggplot(data_agg1, aes(x = year, y = Value)) + 
-      geom_line(aes(color = Variable))  + 
-      guides(scale = "none") + 
-      labs(x = "Year", y = "Value", title = "Preference Music Taste in the Past Century on Spotify") + 
-      scale_x_continuous(limits = c(1921, 2020), breaks = seq(1921, 2020, 10)) +
-      theme_minimal()
-    
-    
+  plot1<- ggplot(data_agg1, aes(x = year, y = Value)) + 
+    geom_line(aes(color = Variable))  + 
+    guides(scale = "none") + 
+    labs(x = "Year", y = "Value", title = "Preference Music Taste in the Past Century on Spotify") + 
+    scale_x_continuous(limits = c(1921, 2020), breaks = seq(1921, 2020, 10)) +
+    theme_minimal()
+  
+  
     ggplotly(plot1)
   })
   
   #WORDCLOUD
   output$wordcloud2 <- renderWordcloud2({
-    spotify_by_genre <- spotify_genre %>% 
+    spotify_by_genre <- spotify_genre %>%
       group_by(genres) %>%
-      summarise(mean_popularity = mean(popularity)) %>% 
-      ungroup()
-    spotify_by_genre$mean_popularity <-round(spotify_by_genre$mean_popularity, digits=2)
-    
-    wordcloud2(data= spotify_by_genre, color = "random-dark", size=0.3, backgroundColor = "white")
+      summarise(mean_popularity = round(mean(popularity), 2)) %>%
+      ungroup()%>% 
+      arrange(desc(mean_popularity)) %>% 
+      head(300)
+
+    wordcloud2(spotify_by_genre, color = "random-dark", size=0.3, backgroundColor = "white")
   })
   
   #OUTPUT PAGE 2
@@ -43,7 +44,7 @@ shinyServer(function(input, output) {
       mutate(text = glue("popularity: {popularity}
                           artists: {artists}")) %>%
       head(15)
-    
+
     plot_rank <- ggplot(data_agg2,
                         aes(x = popularity, y = reorder(name, popularity),
                             text = text)) +
@@ -54,95 +55,97 @@ shinyServer(function(input, output) {
       labs(x = "Popularity (0-100)", y = NULL,
            title = glue("Top 15 Popular Songs {input$year1}")) +
       theme_minimal()
-    
+
     ggplotly(plot_rank, tooltip = "text")
   })
   
-  output$SOTY <- renderValueBox({
-    
-    data_agg2 <- spotify_clean %>%
-      filter(year == input$year1) %>%
-      arrange(desc(popularity)) %>%
-      head(1)
-    
-    valueBox(value = "Song of the Year",
-             subtitle = glue("{data_agg2$name} --- {data_agg2$artists}"),
-             color = "green",
-             icon = icon("trophy"))
-  })
+   output$SOTY <- renderValueBox({
   
-  output$Popularity <- renderValueBox({
-    
-    data_agg2 <- spotify_clean %>%
-      filter(year == input$year1) %>%
-      arrange(desc(popularity)) %>%
-      head(1)
-    
-    valueBox(value = "Popularity Score",
-             subtitle = glue("{data_agg2$popularity} / 100"),
-             color = "green",
-             icon = icon("spotify"))
-  })
+     data_agg2 <- spotify_clean %>%
+       filter(year == input$year1) %>%
+       arrange(desc(popularity)) %>%
+       head(1)
   
+     valueBox(value = "Song of the Year",
+              subtitle = glue("{data_agg2$name} --- {data_agg2$artists}"),
+              color = "green",
+              icon = icon("trophy"))
+   })
+
+   output$Popularity <- renderValueBox({
   
-  ## CORRELATION PLOT
-  output$plot_corr_explicit <- renderPlotly({
-    data_agg_4 <- spotify_clean %>%
-      filter(year == input$year2) %>%
-      arrange(desc(popularity)) %>%
-      head(input$head)
-    
-    plot_dist <- ggplot(data_agg_4,
-                        aes_string(x = input$xlabel, y = input$ylabel)) +
-      geom_jitter(aes(col = as.factor(explicit),
-                      text = glue("{str_to_upper(explicit)}
+     data_agg2 <- spotify_clean %>%
+       filter(year == input$year1) %>%
+       arrange(desc(popularity)) %>%
+       head(1)
+  
+     valueBox(value = "Popularity Score",
+              subtitle = glue("{data_agg2$popularity} / 100"),
+              color = "green",
+              icon = icon("spotify"))
+   })
+  
+   
+   ## CORRELATION PLOT
+   output$plot_corr_explicit <- renderPlotly({
+     data_agg_4 <- spotify_clean %>%
+       filter(year == input$year2) %>%
+       arrange(desc(popularity)) %>%
+       head(input$head)
+     
+     plot_dist <- ggplot(data_agg_4,
+                         aes_string(x = input$xlabel, y = input$ylabel)) +
+       geom_jitter(aes(col = as.factor(explicit),
+                       text = glue("{str_to_upper(explicit)}
                                   Title: {name}
                                   Artists: {artists}
                                   Popularity: {popularity}"))) +
-      labs(x = input$xlabel, y = input$ylabel,
-           title = glue("Distribution of Songs {input$year2}")) +
-      guides(color = FALSE) +
-      theme_minimal()
-    
-    if (input$trend == TRUE) {
-      plot_dist <- plot_dist + geom_smooth()
-    }
-    
-    ggplotly(plot_dist, tooltip = "text")
-  })
-  
-  output$plot_corr_mode <- renderPlotly({
-    data_agg_4 <- spotify_clean %>%
-      filter(year == input$year2) %>%
-      arrange(desc(popularity)) %>%
-      head(input$head)
-    
-    plot_dist <- ggplot(data_agg_4, 
-                        aes_string(x = input$xlabel, y = input$ylabel)) +
-      geom_jitter(aes(col = as.factor(mode)), text = glue("{mode}
+       labs(x = input$xlabel, y = input$ylabel,
+            title = glue("Distribution of Songs {input$year2}")) +
+       guides(color = FALSE) +
+       theme_minimal()
+     
+     if (input$trend == TRUE) {
+       plot_dist <- plot_dist + geom_smooth()
+     }
+     
+     ggplotly(plot_dist, tooltip = "text")
+   })
+   
+   output$plot_corr_mode <- renderPlotly({
+     data_agg_4 <- spotify_clean %>%
+       filter(year == input$year2) %>%
+       arrange(desc(popularity)) %>%
+       head(input$head)
+     
+     plot_dist <- ggplot(data_agg_4, 
+                         aes_string(x = input$xlabel, y = input$ylabel)) +
+       geom_jitter(aes(col = as.factor(mode),
+                       text = glue("{str_to_upper(mode)}
                                   Title: {name}
                                   Artists: {artists}
-                                  Popularity: {popularity}")) +
-      labs(x = input$xlabel, y = input$ylabel, 
-           title = glue("Distribution of Songs {input$year2}")) +
-      guides(color = FALSE) + 
-      theme_minimal()
-    
-    if (input$trend == TRUE) {
-      plot_dist <- plot_dist + geom_smooth()
-    }
-    
-    ggplotly(plot_dist, tooltip = "text")
-  })
-  
-  
-  
-  
-  #OUTPUT PAGE 3
-  output$data <- renderDataTable({
-    DT::datatable(data = spotify, options = list(scrollX=T))
-  })
-  
-  
+                                  Popularity: {popularity}"))) +
+       labs(x = input$xlabel, y = input$ylabel, 
+            title = glue("Distribution of Songs {input$year2}")) +
+       guides(color = FALSE) + 
+       theme_minimal()
+     
+     if (input$trend == TRUE) {
+       plot_dist <- plot_dist + geom_smooth()
+     }
+     
+     ggplotly(plot_dist, tooltip = "text")
+   })
+   
+   
+
+   
+#OUTPUT PAGE 3
+   output$data <- renderDataTable({
+     DT::datatable(data = spotify, options = list(scrollX=T))
+   })
+   
+   
 })
 
+ 
--- ui.R
+++ ui.R
@@ -15,152 +15,152 @@ dashboardPage(
     )
   ),
   
-  # BODY
-  dashboardBody(
-    tabItems(
-      
-      # PAGE 1 #
-      
-      tabItem(tabName = "Trending",
-              fluidPage(
-                h2(tags$b("Spotify: Listening is Everything")),
-                div(style = "text-align:justify",
-                    p("Spotify is a digital music streaming service. Spotify transformed music listening forever when it launched in 2008.
-                       It gives you instant access to its vast online library of music and podcasts, 
-                       allowing you to listen to any content of your choice at any time. 
-                       It is both legal and easy to use. Spotify is available across a range of devices, 
-                       including computers, phones, tablets, speakers, TVs, and cars, and you can easily transition 
-                       from one to another with Spotify Connect. Our Mission is to unlock the potential 
-                       of human creativity—by giving a million creative artists the opportunity to live 
-                       off their art and billions of fans the opportunity to enjoy and be inspired by it."),
-                    br()
-                )
-              ),
-              
-              
-              
-              fluidRow(
-                valueBox("80 Million", "Tracks", icon = icon("music"), color = "red"),
-                valueBox("4.7 Million", "Podcasts", icon = icon("headphones"), color = "purple"),
-                valueBox("183 Millions" , "Markets", icon = icon("globe-asia"), color = "blue"),
-                valueBox("456 Million", "Active Users", icon = icon("play-circle"), color = "green"),
-                valueBox("195 Million", "Subscribers", icon = icon("user"), color = "orange"),
-                valueBox("4 Billion+", "Playlists", icon = icon("record-vinyl"), color = "fuchsia")
-              ),
-              
-              plotlyOutput("plot1"),
-              br(),
-              
-              fluidPage( 
-                h2(tags$b("Genre based on Popularity on Spotify")),
-                wordcloud2Output("wordcloud2"),
-              )
-      ),
-      
-      
-      # PAGE 2 #
-      
-      tabItem(tabName = "stats",
-              
-              fluidPage(
-                
-                box(width = 8,
-                    solidHeader = T,
-                    title = tags$b("Trending Music in a year"),
-                    plotlyOutput("plot_rank")),
-                box(width = 4,
-                    solidHeader = T,
-                    height = 150,
-                    background = "purple",
-                    selectInput(inputId = "year1",
-                                label = "Select Year",
-                                choices = sort(unique(spotify_clean$year), decreasing=TRUE),)
-                ),
-                
-                valueBoxOutput("SOTY", width = 4),
-                valueBoxOutput("Popularity",width = 4),
-                br()
-              ),
-              
-              #correlation plot
-              fluidPage(
-                tabBox(width = 9,
-                       title = tags$b("Songs Correlation on Spotify"),
-                       side = "right",
-                       tabPanel(tags$b("by Mode"),
-                                plotlyOutput("plot_corr_mode", height=480)
-                       ),
-                       tabPanel(tags$b("by Explicitness"),
-                                plotlyOutput("plot_corr_explicit", height=480)
-                       )
-                ),
-                box(width = 3,
-                    solidHeader = T,
-                    background = "purple",
-                    selectInput(inputId = "year2",
-                                label = "Select Year",
-                                choices = unique(spotify_clean$year))
-                ),
-                box(width = 3,
-                    solidHeader = T,
-                    background = "green",
-                    selectInput(inputId = "xlabel",
-                                label = "Select X Axis",
-                                choices = spotify_clean %>%
-                                  select('energy', 'valence', 'acousticness', 'danceability',
-                                         'instrumentalness', 'liveness', 'loudness', 'speechiness', 'tempo') %>%
-                                  names()),
-                    selectInput(inputId = "ylabel",
-                                label = "Select Y Axis",
-                                choices = spotify_clean %>%
-                                  select('loudness', 'valence', 'acousticness', 'danceability',
-                                         'instrumentalness', 'liveness', 'energy', 'speechiness', 'tempo') %>%
-                                  names())
-                ),
-                box(tags$b("Choose whether to display trend"),
-                    width = 3,
-                    solidHeader = T,
-                    background = "green",
-                    checkboxInput("trend", "Display trend", FALSE)
-                ),
-                box(width = 3,
-                    solidHeader = T,
-                    background = "purple",
-                    sliderInput("head", "Number of data", min = 100, max = nrow(spotify_clean), value = 1000)
-                )
-              )
-      ),
-      
-      # PAGE 3
-      
-      tabItem(tabName = "tab_data", 
-              fluidPage(
-                h2(tags$b("Understanding the Data")),
-                br(),
-                dataTableOutput(outputId = "data"),
-                br(),
-                div(style = "text-align:justify", 
-                    p("This dashboard uses a dataset from ", tags$a(href="https://www.kaggle.com/datasets/vatsalmavani/spotify-dataset", "kaggle"), " which is cleaned"),
-                    
-                    br()
-                )
-              )
-      ),
-      
-      # PAGE 3
-      
-      tabItem(tabName = "User_Profile", 
-              fluidPage(
-                h2(tags$b("Profile User")),
-                br(),
-                br(),
-                div(style = "text-align:justify",
-                    p("This project is made by Nur Imam Masri as Capstone Project for completing Data Visualization Specialist in Algoritma Data Science School. You can connect me through ", 
-                      tags$a(href="https://www.linkedin.com/in/nurimammasri/", "linkedin"), 
-                      " and please feel free to hit me up"))))
-      
-    )
-  )
+   # BODY
+   dashboardBody(
+     tabItems(
+       
+       # PAGE 1 #
+       
+       tabItem(tabName = "Trending",
+               fluidPage(
+                 h2(tags$b("Spotify ✨")),
+                 div(style = "text-align:justify",
+                     p("Spotify adalah layanan streaming musik digital. Spotify mengubah mendengarkan musik selamanya ketika diluncurkan pada tahun 2008. Ini memberi Anda akses instan ke perpustakaan musik dan podcast online yang luas, memungkinkan Anda mendengarkan konten apa pun pilihan Anda kapan saja.
+                        Oleh karena itu, Saya ingin menunjukkan bagaimana setiap kontent di kedua platform tersebut dan karakteristiknya, agar beberapa orang yang baru atau sedang ingin menerima hiburan bisa mendapatkan rekomendasi dari dashboard ini :
+                      "),
+                     p(" - Meningkatkan kesadaran masyarakat terhadap entertainments apps"),
+                     p(" - Memberikan informasi mengenai konten di kedua aplikasi (track listening/watching, taste, genre, dll)"),
+                     p(" - Memperlihatkan trending data konten di kedua aplikasi"),
+                     br()
+                     )
+               ),
+               
+               fluidRow(
+                 valueBox("80 Million", "Tracks", icon = icon("music"), color = "red"),
+                 valueBox("4.7 Million", "Podcasts", icon = icon("headphones"), color = "purple"),
+                 valueBox("183 Millions" , "Markets", icon = icon("globe-asia"), color = "blue"),
+                 valueBox("456 Million", "Active Users", icon = icon("play-circle"), color = "green"),
+                 valueBox("195 Million", "Subscribers", icon = icon("user"), color = "orange"),
+                 valueBox("4 Billion+", "Playlists", icon = icon("record-vinyl"), color = "fuchsia")
+               ),
+               
+               fluidRow( 
+                 plotlyOutput("plot1"),
+                 br()
+               )
+       ),
+       
+       
+       # PAGE 2 #
+       
+       tabItem(tabName = "stats",
+               
+               fluidPage(
+                 
+                 box(width = 8,
+                     solidHeader = T,
+                     title = tags$b("Trending Music in a year"),
+                     plotlyOutput("plot_rank")),
+                 box(width = 4,
+                     solidHeader = T,
+                     height = 150,
+                     background = "purple",
+                     selectInput(inputId = "year1",
+                                 label = "Select Year",
+                                 choices = sort(unique(spotify_clean$year), decreasing = TRUE),)
+                 ),
+                 
+                 valueBoxOutput("SOTY", width = 4),
+                 valueBoxOutput("Popularity",width = 4),
+                 br()
+               ),
+               
+               fluidPage(
+                 h2(tags$b("Genre based on Popularity on Spotify")),
+                 wordcloud2Output("wordcloud2"),
+                 br()
+               ),
+               
+               
+               #correlation plot
+               fluidPage(
+                 tabBox(width = 9,
+                        title = tags$b("Songs Correlation on Spotify"),
+                        side = "right",
+                        tabPanel(tags$b("by Mode"),
+                                 plotlyOutput("plot_corr_mode", height=480)
+                        ),
+                        tabPanel(tags$b("by Explicitness"),
+                                 plotlyOutput("plot_corr_explicit", height=480)
+                        )
+                 ),
+                 box(width = 3,
+                     solidHeader = T,
+                     background = "purple",
+                     selectInput(inputId = "year2",
+                                 label = "Select Year",
+                                 choices = sort(unique(spotify_clean$year), decreasing = TRUE))
+                 ),
+                 box(width = 3,
+                     solidHeader = T,
+                     background = "green",
+                     selectInput(inputId = "xlabel",
+                                 label = "Select X Axis",
+                                 choices = spotify_clean %>%
+                                   select('energy', 'valence', 'acousticness', 'danceability',
+                                          'instrumentalness', 'liveness', 'loudness', 'speechiness', 'tempo') %>%
+                                   names()),
+                     selectInput(inputId = "ylabel",
+                                 label = "Select Y Axis",
+                                 choices = spotify_clean %>%
+                                   select('loudness', 'valence', 'acousticness', 'danceability',
+                                          'instrumentalness', 'liveness', 'energy', 'speechiness', 'tempo') %>%
+                                   names())
+                 ),
+                 box(tags$b("Choose whether to display trend"),
+                     width = 3,
+                     solidHeader = T,
+                     background = "green",
+                     checkboxInput("trend", "Display trend", FALSE)
+                 ),
+                 box(width = 3,
+                     solidHeader = T,
+                     background = "purple",
+                     sliderInput("head", "Number of data", min = 100, max = nrow(spotify_clean), value = 1000)
+                 )
+               )
+       ),
+       
+       # PAGE 3
+       
+       tabItem(tabName = "tab_data", 
+               fluidPage(
+                 h2(tags$b("Understanding the Data")),
+                 br(),
+                 dataTableOutput(outputId = "data"),
+                 br(),
+                 div(style = "text-align:justify", 
+                     p("This dashboard uses a dataset from ", tags$a(href="https://www.kaggle.com/datasets/vatsalmavani/spotify-dataset", "kaggle"), " which is cleaned"),
+                     
+                     br()
+                 )
+               )
+       ),
+       
+       # PAGE 3
+       
+       tabItem(tabName = "User_Profile", 
+               fluidPage(
+                 h2(tags$b("Profile User")),
+                 br(),
+                 br(),
+                 div(style = "text-align:justify",
+                     p("This project is made by Nur Imam Masri as Capstone Project for completing Data Visualization Specialist in Algoritma Data Science School. You can connect me through ", 
+                       tags$a(href="https://www.linkedin.com/in/nurimammasri/", "linkedin"), 
+                       " and please feel free to hit me up"))))
+       
+     )
+   )
 )
 
 
